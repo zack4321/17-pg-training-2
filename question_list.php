0340405f42e2b74a4abd4197f4e0c1cc581667da
sossii <sossii@sossii-MacBook.local>
2017-04-17 15:53:31 +0900

    お題一覧画面を若干それっぽく

--- question_list.php
+++ question_list.php
@@ -3,16 +3,34 @@ require_once 'functions.php';
 // お題一覧ページ
 
 $database = getDatabase();
+
+// お題一覧を取得
 $questions = $database->query("
     SELECT *
     FROM `question`
     ORDER BY `created_at` DESC
 ")->fetchAll(PDO::FETCH_ASSOC);
 
+foreach ($questions as $i => $question) {
+    $user = $database->query("
+        SELECT *
+        FROM `user`
+        WHERE `id` = {$question['user_id']}
+    ")->fetch(PDO::FETCH_ASSOC);
+
+    $questions[$i]['user'] = $user;
+}
+
 ?>
+
+<link rel="stylesheet" href="/css/style.css">
 
 <div>お題一覧</div>
 <?php foreach($questions as $question): ?>
-    <div><?= $question['user_id'] ?></div>
-    <img src="/image/<?= $question['image_file_name'] ?>">
+    <a href="question.php?id=<?= $question['id'] ?>">
+        <div class="question-item">
+            <div><?= $question['user']['name'] ?>さんのお題</div>
+            <img src="/image/<?= $question['image_file_name'] ?>" width="300px">
+        </div>
+    </a>
 <?php endforeach; ?>
