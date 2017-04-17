4e7a432f76d4d59c5485684416493a680036170d
sossii <sossii@sossii-MacBook.local>
2017-04-17 20:32:03 +0900

    tootを並べる

--- index.php
+++ index.php
@@ -1,6 +1,38 @@
 <?php
 // トップページ
+require_once 'functions.php';
+
+session_start();
+redirectToLoginPageIfNotLoggedIn();
+
+$database = getDatabase();
+
+$toots = $database->query("
+    SELECT *
+    FROM `toot`
+    ORDER BY `created_at` DESC
+")->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($toots as $i => $toot) {
+    $user = $database->query("
+        SELECT *
+        FROM `user`
+        WHERE `id` = {$toot['user_id']}
+    ")->fetch(PDO::FETCH_ASSOC);
+
+    $toots[$i]['user'] = $user;
+}
 
 ?>
-<a href="/add_question.php">お題投稿</a>
-<a href="/question_list.php">お題一覧</a>
+
+<?php foreach($toots as $toot) { ?>
+    <a href="toot.php?id=<?= $toot['id'] ?>">
+        <div class="question-item">
+            <div><?= $toot['user']['name'] ?></div>
+            <div><?= $toot['text'] ?></div>
+            <?php if ($toot['image_file_name'] !== '') { ?>
+                <img src="/image/<?= $toot['image_file_name'] ?>" width="300px">
+            <?php } ?>
+        </div>
+    </a>
+<?php } ?>
--- init.sql
+++ init.sql
@@ -10,19 +10,11 @@ CREATE TABLE `database`.`user`(
   PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `database`.`question`(
+CREATE TABLE `database`.`toot`(
   `id` int AUTO_INCREMENT,
   `user_id` int,
-  `image_file_name` varchar(100),
-  `created_at` datetime,
-  PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `database`.`answer`(
-  `id` int AUTO_INCREMENT,
-  `user_id` int,
-  `question_id` int,
   `text` varchar(500),
+  `image_file_name` varchar(100),
   `created_at` datetime,
   PRIMARY KEY (`id`)
 );
@@ -31,11 +23,8 @@ INSERT INTO `database`.`user` (`name`) VALUES
   ('sossii'),
   ('soshio');
 
-INSERT INTO `database`.`question` (`user_id`, `image_file_name`, `created_at`) VALUES
-  (1, '00000000000000000000000000000000.png', '2017-04-13 00:00:00'),
-  (2, '00000000000000000000000000000001.png', '2017-04-13 01:00:00');
+INSERT INTO `database`.`toot` (`user_id`, `text`,`image_file_name`, `created_at`) VALUES
+  (1, 'わっしょい', '00000000000000000000000000000000.png', '2017-04-13 00:00:00'),
+  (2, '最高', '', '2017-04-13 01:00:00'),
+  (2, 'みそしる', '', '2017-04-13 01:00:00');
 
-INSERT INTO `database`.`answer` (`user_id`, `question_id`, `text`, `created_at`) VALUES
-  (1, 1,'答えです', '2017-04-14 00:00:00'),
-  (2, 1, '答えです2', '2017-04-14 01:00:00'),
-  (2, 1, '答えです3', '2017-04-14 02:00:00');
