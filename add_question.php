96fed13b073f84900225cdace9fc06d31f9dfb2d
sossii <sossii@sossii-MacBook.local>
2017-04-17 16:08:23 +0900

    投稿ページのURL変える

--- add_question.php
+++ add_question.php
@@ -1,25 +1,9 @@
 <?php
-require_once 'functions.php';
-// お題投稿
+// お題投稿ページ
 
-$user_id = 0;
-$created_at = date('Y-m-d H:i:s');
-
-$image_file_name = md5(mt_rand()) . '.' . pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
-$upload_path = dirname(__FILE__) . '/image/' . $image_file_name;
-move_uploaded_file($_FILES['image']['tmp_name'], $upload_path);
-
-$database = getDatabase();
-$database->query("
-    INSERT INTO `question` (
-        `user_id`,
-        `image_file_name`,
-        `created_at`
-    ) VALUES (
-        {$user_id},
-        '{$image_file_name}',
-        '{$created_at}'
-    )
-");
-
-header('Location: /question_list.php');
+?>
+<div>お題投稿</div>
+<form enctype="multipart/form-data" method="post" action="/add_question_submit.php">
+    <input type="file" name="image" required>
+    <input type="submit" value="送信する">
+</form>
--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 // トップページ
 
 ?>
-<a href="/add_question_form.php">お題投稿</a>
+<a href="/add_question.php">お題投稿</a>
 <a href="/question_list.php">お題一覧</a>
