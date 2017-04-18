a65e23b71f77056dfae0047aefd8bfc4d86f1250
sossii <sossii@sossii-MacBook.local>
2017-04-18 22:59:25 +0900

    ユーザー新規登録

--- login.php
+++ login.php
@@ -10,3 +10,10 @@
     <input name="login_name" required>
     <input type="submit" value="ログイン">
 </form>
+
+<div>新規登録</div>
+<form method="post" action="/post_signup.php">
+    ID @<input name="login_name" required>
+    表示名<input name="display_name" required>
+    <input type="submit" value="新規登録">
+</form>
--- post_signup.php
+++ post_signup.php
@@ -1,37 +1,37 @@
 <?php
 require_once 'functions.php';
-// トゥート投稿
+// ユーザー新規登録
 
-session_start();
-redirectToLoginPageIfNotLoggedIn();
+$login_name = $_POST['login_name'];
+$display_name = $_POST['display_name'];
 
-$user_id = $_SESSION['user_id'];
-
-$text = $_POST['text'];
-
-if ($_FILES['image']['name'] === '') {
-    $image_file_name = '';
-} else {
-    $image_file_name = md5(mt_rand()) . '.' . pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
-    $upload_path = dirname(__FILE__) . '/image/' . $image_file_name;
-    move_uploaded_file($_FILES['image']['tmp_name'], $upload_path);
-}
-
-$created_at = date('Y-m-d H:i:s');
+// if ($_FILES['image']['name'] === '') {
+//     $image_file_name = '';
+// } else {
+//     $image_file_name = md5(mt_rand()) . '.' . pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+//     $upload_path = dirname(__FILE__) . '/image/' . $image_file_name;
+//     move_uploaded_file($_FILES['image']['tmp_name'], $upload_path);
+// }
 
 $database = getDatabase();
-$database->query("
-    INSERT INTO `user` (
-        `user_id`,
-        `text`,
-        `image_file_name`,
-        `created_at`
-    ) VALUES (
-        {$user_id},
-        '{$text}',
-        '{$image_file_name}',
-        '{$created_at}'
-    )
-");
 
-header('Location: /');
+$user = $database->query("
+    SELECT *
+    FROM `user`
+    WHERE `login_name` = '{$login_name}'
+")->fetch(PDO::FETCH_ASSOC);
+
+if ($user === false) {
+    // 同じlogin_nameを持つユーザーがいなかったら新規登録
+    $database->query("
+        INSERT INTO `user` (
+            `login_name`,
+            `display_name`
+        ) VALUES (
+            '{$login_name}',
+            '{$display_name}'
+        )
+    ");
+}
+
+header('Location: /login.php');
