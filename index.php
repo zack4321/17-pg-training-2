da0aa9abf34d4af270564980ec7c5885307c979c
Zack <zack@pixiv.com>
2017-05-02 12:34:16 +0900

    トゥートぽくしたよ

--- index.php
+++ index.php
@@ -11,3 +11,57 @@ $database = getDatabase();
 ?>
 
 <!-- 下にindex.htmlをコピペして、index.htmlを消そう！ -->
+<html>
+    <head>
+        <title>Yastodon(ヤストドン)</title>
+        <link rel="stylesheet" href="/css/style.css">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="container myself-container">
+                <div class="myself">
+                    <div class="user-icon"></div>
+                    <div class="user-name"></div>
+                </div>
+                <form enctype="multipart/form-data" method="post" action="/post_toot.php">
+                    <textarea name="text" placeholder="今なにしてる？" required></textarea>
+                    <input type="file" name="image">
+                    <div class="toot-button-container">
+                        <input type="submit" class="toot-button" value="トゥート!">
+                    </div>
+                </form>
+            </div>
+
+            <div class="container toot-container">
+                <div class="label icon-home"><img class="label-icon" src="/img/home.png" width="15" alt="Home - ">ホーム</div>
+                <div class="toot">
+                  <div class="toot-text">
+                    <div class="toot-user">
+                      <div class="toot-user-icon"><img src="https://files.slack.com/files-tmb/T02541Q7U-F55EULAEB-5f5a012488/image_uploaded_from_ios_1024.jpg"></div>
+                      <div class="toot-user-name">zack</div>
+                      <div class="toot-user-id">@zack</div>
+                    </div>
+                    <div class="toot-body">
+                      zackだお
+                    </div>
+                  </div><!-- toot-text -->
+                  <div class="toot-image">
+                    <img src="/img/riza-don.jpg">
+                  </div>
+                </div> <!-- toot -->
+            </div>
+
+
+            <div class="container about-container">
+                <div class="label icon-asterisk"><img class="label-icon" src="/img/asterisk.png" width="15" alt="Start - ">スタート</div>
+                <div class="contents">
+                    <p>
+                        Yastodonとは研修のために作られた教育用ソーシャル・ネットワーキング・サービスです。<br>
+                        あなただけの素敵なサービスをここから成長させていってください。
+                    </p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
